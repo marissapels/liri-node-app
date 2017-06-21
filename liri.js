fd46598f3e5b3c403e969461587ca03e6065e950
Marissa Pels <opsadmin@Opss-MacBook-Pro-3.local>
2017-06-21 13:48:26 -0500

    twitter update

--- liri.js
+++ liri.js
@@ -1,8 +1,21 @@
+// var request=require("request");
+var Twitter = require("twitter");
+var keys=require("./keys.js");
+var client = new Twitter(keys.twitterKeys);
+
 var commands = {
-	"my-tweets":
-	"spotify-this-song":
-	"movie-this":
-	"do-what-it-says":
+	"my-tweets":function(){
+
+		var params = {screen_name: "marpels12"};
+		client.get("statuses/user_timeline", params, function(error, tweets, response) {
+			if (!error) {
+				console.log(error);
+			}
+			for (var i=0; i<tweets.length; i++){
+				console.log(i+1+". "+tweets[i].text+"  (Created: "+tweets[i].created_at+")\n");
+			}
+		});
+	}
 }
 
-commands[process.argv[2]]();
+commands["my-tweets"]();
