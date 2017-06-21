e075b24bbcbd36388ea0ef2853e06fafae5879af
Marissa Pels <marissapels@gmail.com>
2017-06-21 08:03:58 -0500

    upload

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'sg82ZWiLy5tOYU1WWvcbcgzoK',
+  consumer_secret: 'c9pRrJzdivugFI22DO7Sifan3jD1lGXSLaEMylxw9NsypDEQUj',
+  access_token_key: '	2367360336-zGfUj6518VvyG4Zz1jBro9LUn8vTtG1Ny8KN39A',
+  access_token_secret: 'EB0aDpG9Ibbbah7Y89xi2nEyr9FRA77WgTyBdjHpgq7Tn',
+}
---
+++ liri.js
@@ -0,0 +1,8 @@
+var commands = {
+	"my-tweets":
+	"spotify-this-song":
+	"movie-this":
+	"do-what-it-says":
+}
+
+commands[process.argv[2]]();
